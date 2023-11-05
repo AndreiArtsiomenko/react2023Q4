974b63a6d5a5a1a428dfa33597ab1e3ff6f41819
Andrei Artsiomenko <andartsiomenko@gmail.com>
2023-11-06 01:32:41 +0300

    fix: change style in card-person

--- src/components/CardPerson/CardPerson.tsx
+++ src/components/CardPerson/CardPerson.tsx
@@ -4,11 +4,8 @@ import { PeopleType } from '../../types/types';
 export default function CardPerson({ person }: { person: PeopleType }) {
   return (
     <div className={style.person_cards}>
-      <h2>{person.name}</h2>
-      <p>Birth year: {person.birth_year}</p>
+      <h3>{person.name}</h3>
       <p>Gender: {person.gender}</p>
-      <p>Mass: {person.mass}kg</p>
-      <p>Height: {person.height}</p>
     </div>
   );
 }
