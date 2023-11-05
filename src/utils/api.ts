b1a5ba834099d85764ae36007fb75c3204125384
Andrei Artsiomenko <andartsiomenko@gmail.com>
2023-11-05 21:45:49 +0300

    feat: add routing

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,16 @@
-export default async function getData(search: string) {
+import { LoaderFunction } from 'react-router-dom';
+
+const getPeople = async (search: string) => {
   const url = 'https://swapi.dev/api/people/?search=';
   const response = await fetch(`${url}${search}&page=${1}`);
   const data = await response.json();
   return data.results;
-}
+};
+
+const getPerson: LoaderFunction = async ({ params }) => {
+  const response = await fetch(`https://swapi.dev/api/people/${params.id}`);
+  const data = await response.json();
+  return data;
+};
+
+export { getPeople, getPerson };
