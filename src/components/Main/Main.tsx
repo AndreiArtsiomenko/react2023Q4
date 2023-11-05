1a82103b059981381c555bbf3ac78e75a16d52eb
Andrei Artsiomenko <andartsiomenko@gmail.com>
2023-11-05 22:35:03 +0300

    feat: add routing in main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,19 +1,31 @@
 import style from './Main.module.css';
+import { Link, Outlet, useNavigation, useOutletContext } from 'react-router-dom';
 import { PeopleType } from '../../types/types';
 import CardPerson from '../CardPerson/CardPerson';
+import Loading from '../Loading/Loading';
 
-export default function Main({ people }: { people: PeopleType[] }) {
+export default function Main() {
+  const people = useOutletContext<PeopleType[]>();
+  const navigation = useNavigation();
   return (
     <main className={style.main}>
       {people.length ? (
         <div className={style.people}>
-          {people.map((person) => (
-            <CardPerson person={person} key={person.name} />
-          ))}
+          {people.map((person) => {
+            const id = person.url.split('/')[5];
+            return (
+              <Link to={`people/${id.toString()}`} key={person.name}>
+                <CardPerson person={person} />
+              </Link>
+            );
+          })}
         </div>
       ) : (
         <h2>Not Found</h2>
       )}
+      <div className={style.details}>
+        {navigation.state === 'loading' ? <Loading /> : <Outlet />}
+      </div>
     </main>
   );
 }
