3ac7d0af7873b08f4880ad8be3cacf69bae513e0
Andrei Artsiomenko <andartsiomenko@gmail.com>
2023-11-13 01:39:36 +0300

    feat: add page404 and test

---
+++ src/components/Page404/Page404.test.tsx
@@ -0,0 +1,15 @@
+import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from '../../App';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('', () => {
+  test('landing on a bad page', () => {
+    render(
+      <MemoryRouter initialEntries={['/3']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/404/i)).toBeInTheDocument();
+  });
+});
